eb20eba7601dead089f9fcab8479ea10e89e4aff
DadhichTanishka <tanishka.dadhich@kalvium.community>
2023-03-22 11:05:28 +0530

    changed the color of nav of landing page

--- frontend/src/components/homenav.js
+++ frontend/src/components/homenav.js
@@ -36,7 +36,7 @@ function ResponsiveAppBar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar sx={{backgroundColor:"black"}} position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
