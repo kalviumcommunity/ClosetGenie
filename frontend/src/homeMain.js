b3c00696c41eed95c86cd06569d445f79fb7b4c5
DadhichTanishka <tanishka.dadhich@kalvium.community>
2023-04-17 17:00:32 +0530

    implemented toastify

--- frontend/src/homeMain.js
+++ frontend/src/homeMain.js
@@ -2,7 +2,9 @@ import NavBar from "./components/navbar";
 import Carousels from "./components/carousels";
 import OverviewPrefinder from "./components/prefinderoverview";
 import Category from "./components/category";
+
 function Main(){
+    
     return(
         <div id="main">
          <NavBar/>
