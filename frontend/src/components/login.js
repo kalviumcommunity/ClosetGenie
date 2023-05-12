e32fb56697bdc25d83398941774b95c1578c0bc0
DadhichTanishka <tanishka.dadhich@kalvium.community>
2023-05-12 14:58:36 +0530

    fixed auth0 redirect

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,10 +1,21 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+// import { useNavigate } from "react-router-dom";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button  onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button  onClick={() => loginWithRedirect({
+    appState:{
+      returnTo: window.location.pathname
+    },
+  })}>Log In</button>;
 };
+
+// export const useRedirectCallback=(appState)=>{
+//   console.log(appState)
+//   const navigate=useNavigate()
+//   navigate(appState.returnTo)
+// }
 
 export default LoginButton;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,15 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Router } from 'react-router-dom';
+// import {useRedirectCallback} from "./components/login"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 // {console.log("Window href : ",window.location.href)}
 root.render(
-    <Auth0Provider
-    domain={process.env.REACT_APP_domain}
-    clientId={process.env.REACT_APP_clientId}
-    
-    authorizationParams={{
-      redirect_uri: window.location.href
-    }}
-  > 
-    <App />
-
-    </Auth0Provider>
+  // <Router>
+  <BrowserRouter>
+    <App /></BrowserRouter>
+    // </Router> 
 );
